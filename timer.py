7699e2c372a149adf25aa7d8ad5ce03c437bcf2f
rebussharma <37812264+rebussharma@users.noreply.github.com>
2022-05-01 14:24:26 -0500

    fixed unused imports
    Unused imports were givin errors in task scheduler, had to take them out.

--- timer.py
+++ timer.py
@@ -1,11 +1,9 @@
-import selenium.common.exceptions
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
-from webdriver_manager.chrome import ChromeDriverManager
 import time
 from datetime import date, datetime, timedelta
 import os
